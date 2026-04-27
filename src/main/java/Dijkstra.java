fe08f25317817c449e61afda9143f1baab0ce754
Davi Schuchowsky <romarioprrto@gmail.com>
2026-04-27 20:05:54 -0300

    Add files via upload

---
+++ src/main/java/Dijkstra.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmo_dijkstra;
+
+/**
+ *
+ * @author Admin
+ */
+public class Algoritmo_dijkstra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
